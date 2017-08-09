c632e183b0df56419a8aa157ff01c9f8eccdc392
SenhW <senhwang3@gmail.com>
2017-08-09 17:24:47 -0400

    Set up authentication routes for registering new users

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session") ({
+	secret: "Test secret",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res) {
 	res.render("landing");
 });
@@ -107,6 +119,29 @@ app.post("/restaurants/:id/comments", function(req, res) {
 				}
 			});
 		}
+	});
+});
+
+// ==============
+// AUTH ROUTES
+// ==============
+
+// Show register form
+app.get("/register", function(req, res) {
+	res.render("register");
+});
+
+// Handle sign up logic
+app.post("/register", function(req, res) {
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user) {
+		if(err) {
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function() {
+			res.redirect("/restaurants");
+		});
 	});
 });
 
