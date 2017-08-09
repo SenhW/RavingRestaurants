d6a17c8aacc0ada64b6e5064c7b6cd0c410d77b5
SenhW <senhwang3@gmail.com>
2017-08-09 17:54:05 -0400

    Set up login route

--- app.js
+++ app.js
@@ -145,6 +145,19 @@ app.post("/register", function(req, res) {
 	});
 });
 
+// Show login form
+app.get("/login", function(req, res) {
+	res.render("login");
+});
+
+// Handling login logic
+app.post("/login", passport.authenticate("local", 
+	{
+		successRedirect: "/restaurants",
+		failureRedirect: "/login"
+	}), function(req, res) {
+});
+
 app.listen(3000, function() {
 	console.log("Server has started!");
 });
