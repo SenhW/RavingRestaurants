25a71b9a3caac9ad393d529b2c9908698c3347af
SenhW <senhwang3@gmail.com>
2017-08-11 17:01:03 -0400

    Associate and display author of a new restaurant

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -20,7 +20,11 @@ router.post("/", isLoggedIn, function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var newRestaurant = {name: name, image: image, description: desc}
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	var newRestaurant = {name: name, image: image, description: desc, author: author}
 	// Create a new restaurant and save to database
 	Restaurant.create(newRestaurant, function(err, newlyCreated) {
 		if(err) {
