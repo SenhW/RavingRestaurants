58f0e625c609e18a2b817d541b633cadf8fa946e
SenhW <senhwang3@gmail.com>
2017-08-11 23:46:14 -0400

    Allow only users who own comment to be able to view edit and delete buttons

--- routes/comments.js
+++ routes/comments.js
@@ -46,7 +46,7 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 // EDIT - show form to edit comment
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
 	Comment.findById(req.params.comment_id, function(err, foundComment) {
 		if(err) {
 			res.redirect("back");
@@ -57,7 +57,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // UPDATE - Edit comment into database
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
 		if(err) {
 			res.redirect("back");
@@ -68,7 +68,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // DESTROY - Delete comment from database
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
 	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
 		if(err) {
 			res.redirect("back");
@@ -84,6 +84,30 @@ function isLoggedIn(req, res, next) {
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+	// Is user logged in?
+	if(req.isAuthenticated()) {
+		Comment.findById(req.params.comment_id, function(err, foundComment) {
+			if(err) {
+				res.redirect("back")
+			} else {
+				// Does user own the comment?
+				if(foundComment.author.id.equals(req.user._id)) {
+					next();
+				} 
+				// Otherwise, redirect
+				else {
+					res.redirect("back");
+				}
+			}
+		});
+	} 
+	// If not, redirect	
+	else {
+		res.redirect("back");
+	}
 }
 
 module.exports = router;
