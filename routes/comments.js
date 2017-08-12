657ac05d8ecabe2088ed5f1df1b2db42144290b1
SenhW <senhwang3@gmail.com>
2017-08-11 23:30:41 -0400

    Add destroy comment route

--- routes/comments.js
+++ routes/comments.js
@@ -67,6 +67,17 @@ router.put("/:comment_id", function(req, res) {
 	});
 });
 
+// DESTROY - Delete comment from database
+router.delete("/:comment_id", function(req, res) {
+	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+		if(err) {
+			res.redirect("back");
+		} else {
+			res.redirect("/restaurants/" + req.params.id);
+		}
+	});
+});
+
 // Middleware
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()) {
