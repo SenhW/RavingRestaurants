cf7dc8ec7d8365f31402da6d6516336edb4a1983
SenhW <senhwang3@gmail.com>
2017-08-14 02:35:46 -0400

    Make nav bar menu look neater and collapse menu in mobile view

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res) {
 		if(err) {
 			console.log(err);
 		} else {
-			res.render("restaurants/index", {restaurants:allRestaurants})
+			res.render("restaurants/index", {restaurants: allRestaurants, page: 'restaurants'});
 		}
 	});
 });
