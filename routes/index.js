8523ec409500e7f2c2b4a9eb37abdd00f20a0665
SenhW <senhwang3@gmail.com>
2017-08-14 02:45:02 -0400

    Modified register route flash message

--- routes/index.js
+++ routes/index.js
@@ -18,11 +18,10 @@ router.post("/register", function(req, res) {
 	var newUser = new User({username: req.body.username});
 	User.register(newUser, req.body.password, function(err, user) {
 		if(err) {
-			req.flash("error", err.message);
-			return res.redirect("/register");
+			return res.render("register", {error: err.message});
 		}
 		passport.authenticate("local")(req, res, function() {
-			req.flash("success", "Welcome to RavingRestaurants " + user.username + "!");
+			req.flash("success", "Sucessfully Signed Up! Nice to meet you " + user.username + "!");
 			res.redirect("/restaurants");
 		});
 	});
