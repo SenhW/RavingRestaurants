ddce02f38a0075ad7f0d47659d9ff4da9b54e188
Senh Wang <senhwang3@gmail.com>
2017-08-15 18:56:59 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.google_api = /** Add Google Maps API here **/;
+config.email = /** Add email address here **/;
+config.password = /** Add email password here **/;
+config.mlab_username = /** Add mLab username here **/;
+config.mlab_password = /** Add mLab password here **/;
+
+module.exports = config;
